dbf7fc774a9c9a84804836aef74d38a00809e9c1
William Aguera <williamaguera.m@hotmail.com>
2025-03-23 21:47:52 -0300

    editar tarefa

--- script.js
+++ script.js
@@ -1,49 +1,118 @@
 const localStorageKey = 'tasks'
-function validacaoNovaTarefa(){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-    let inputValue = document.getElementById('input-new-task').value
-    let exists = values.find(x => x.name == inputValue)
-    return !exists ? false : true 
 
-}
-function newTask() {
+
+let editing = false;
+let editItemArray = null;
+
+// Tarefa MODEL - MODEL
+
+// id number
+// description String
+// status String - pending, done
+
+// Dont Repeat Yourself
+
+function saveTask() {
     let input = document.getElementById('input-new-task')
-    
         input.style.border = ''
-  
 
     //validação
     if(!input.value) {
-
         input.style.border = '1px solid red'
         alert('Digite uma tarefa')
+        return; // Para a execucão da funcao
     }
-    else if(validacaoNovaTarefa()){
-        alert('Já existe uma tafera com essa descrição!')
-
-    }
-    else{
-    //armazendo no localStorage
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+
+
+    // Salva tarefa existente
+    if (editing) {
+
+        // editar item com nova descricao
+        editItemArray.description = input.value
+
+
+        const mapTmp = values.map((item) => {
+            return item.id == editItemArray.id ? editItemArray : item
+        })
+
+
+        // filtrar para excluir item antigo
+        // const tmp = values.filter((item) => {
+        //     return item.id != editItemArray.id
+        // })
+        //
+        // // adicionar item editado
+        // tmp.push(editItemArray)
+
+
+        // Salvar no banco
+        localStorage.setItem(localStorageKey, JSON.stringify(mapTmp))
+
+
+        // atualizar dados na tela
+        showValues()
+
+        // limpar o campo
+        input.value = ''
+
+
+        // alterar label do botao
+        const textBtn = document.getElementsByClassName('add-text')[0]
+        textBtn.innerHTML = 'Adicionar'
+
+        editing = false;
+        return;
+    }
+
+
+    // Salva nova tarefa
     values.push({
-        name: input.value
+        id: new Date().getTime(),
+        description: input.value,
+        status: 'pending',
     })
+
     localStorage.setItem(localStorageKey,JSON.stringify(values))
     showValues()
-    }
 
   input.value = ''
 }
 
 function showValues(){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let array = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
     let list = document.getElementById('to-do-list')
     list.innerHTML = ''
-    for(let i = 0; i < values.length; i++) {
-        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
-</svg></button></li>`
+    for (let i = 0; i < array.length; i++) {
+        list.innerHTML += `<li>${array[i]['description']}
+            <button id='btn-ok' onclick='removeItem("${array[i]['description']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+              <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+             </svg>
+            </button>
+            <button onclick=editItem("${array[i]['id']}") class="edit-button">
+                Edit
+            </button>
+          </li>`
     }
+}
+
+function editItem(id) {
+
+    // buscar item no array
+    editing = true;
+    let array = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    const find = array.find((item) => {
+        return item.id == id;
+    })
+    editItemArray = find;
+
+    // alterar label do botao
+    const textBtn = document.getElementsByClassName('add-text')[0]
+    textBtn.innerHTML = 'Salvar'
+
+    // adicionar novo item ao texto do input
+    let input = document.getElementById('input-new-task')
+    input.value = find.description
 }
 
 function removeItem(data){
